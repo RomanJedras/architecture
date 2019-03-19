2b6893cb20826e1fb730d0d093d937c8f00e4b81
Roman Jędras <romanjedras@o2.pl>
2019-03-19 19:07:58 +0100

    Applay sliader carousel Ficker

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+$( document ).ready(function() {
+
+   let $carousel = $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+        pageDots: false,
+        hash: true
+    });
+
+    $carousel.on( 'scroll.flickity', function( event, progress ) {
+        $('#progress').width( progress * 100 + '%' );
+    });
+
+    $('#reset').on('click', function () {
+        $carousel.flickity('previous');
+    });
+});
+
+
+
+
